96e877dfea7af653567c8b72166a7a535f7f4ec9
Queue2109 <helena@LAPTOP-4FPK93GP.localdomain>
2022-04-06 00:54:19 +0200

    Vaje 5

--- VAJE/VAJE_05/vaje05/naloga.c
+++ VAJE/VAJE_05/vaje05/naloga.c
@@ -5,21 +5,56 @@
 #include "naloga.h"
 
 int vsota(int* zac, int* kon) {
-    // popravite / dopolnite ...
-    
-    return -1;
+    int vsotaEl = 0;
+    /*int *pi;
+    pi = &(zac[0]);
+    while(pi != kon) {
+        vsotaEl += *pi;
+        pi++;
+    }*/
+    while(zac <= kon) {
+        vsotaEl += *zac;
+        zac++;
+    }
+    return vsotaEl;
 }
 
 void indeksInKazalec(int* t, int* indeks, int** kazalec) {
-    // dopolnite ...
+   
+    if(*indeks == -1) {
+        int i = 0;
+        while(1) {
+            if((t + i) == *kazalec) {
+                *indeks = i;
+                break;
+            }
+            i++;
+        } 
+    } else {
+        *kazalec = t + *indeks;
+    }
+    
 }
 
 void frekvenceCrk(char* niz, int** frekvence) {
-    // dopolnite ...
+    char *p = niz;
+
+    int *pogostosti = calloc(26, sizeof(int));
+
+    while(*p != '\0'){
+        char znak = *p;
+
+        if((znak >= 'A' && znak <= 'Z') || (znak >= 'a' && znak <= 'z')){
+            int indeks = (znak >= 'a') ? (znak - 'a') : (znak - 'A');
+            pogostosti[indeks]++;
+        } 
+        p++;
+    }
+    *frekvence = pogostosti;
 }
 
 int main() {
     // koda za ro"cno testiranje (po "zelji)
-
+  
     return 0;
 }
