1a7a65a5162db712c0db2529bb7a2183ab40ca01
Queue2109 <hj2109@student.uni-lj.si>
2022-06-19 16:24:04 +0200

    Vaje08

--- VAJE/VAJE_08/nahrbtnik1/nahrbtnik1.c
+++ VAJE/VAJE_08/nahrbtnik1/nahrbtnik1.c
@@ -1,148 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct predmet {
-    int prostornina;
-    int cena;
-} predmet;
+int MEMO[1001][1001];
 
-typedef struct _node {
-    struct predmet predmet;
-    struct _node *next;
-} node;
+int backpack(int numOfEl, int volumes[], int prices[], int index, int volume)
+{
+    if (index == numOfEl)
+        return 0;
 
-typedef node *list;
+    // Ce smo vrednost backpack(..., index, volume) ze izracunali
+    if (MEMO[index][volume] > 0)
+        return MEMO[index][volume];
+    int najCena = backpack(numOfEl, volumes, prices, index + 1, volume);
 
-void printRek(node *lst) {
-    if(lst == NULL) {
-        printf("\n");
-        return;
+    if (volumes[index] <= volume)
+    {
+        int c = prices[index] + backpack(numOfEl, volumes, prices, index + 1, volume - volumes[index]);
+        if (c > najCena)
+            najCena = c;
     }
-    printf("%d %d\n", lst->predmet.prostornina, lst->predmet.cena);
-    printRek(lst->next);
+    MEMO[index][volume] = najCena;
+    return najCena;
 }
+int main(int argc, char const *argv[])
+{
+    // Input:
+    /*
+    12
+    5
+    3 4 7 2 3
+    5 6 8 9 1
+    */
 
-node *insertElement(struct predmet predmet, node *seznam) {
-    if(seznam == NULL) {
-        seznam = (node*) malloc(sizeof(node));
-        seznam->predmet = predmet;
-        seznam->next = NULL;
-        return seznam;
-    } else {
-        node *l = seznam;
-        while(l->next != NULL) {
-            l = l->next;
-        }
-        l->next = (node*)malloc(sizeof(node));
-        l = l->next;
-        l->predmet = predmet;
-        l->next = NULL;
-        return seznam;
+    int volume, numOfObjects;
+    scanf("%d %d", &volume, &numOfObjects);
+    int volumes[numOfObjects];
+    int prices[numOfObjects];
+    for (int i = 0; i < numOfObjects; i++)
+    {
+        scanf("%d", &volumes[i]);
     }
-}
-
-node *deleteElement(node *seznam, struct predmet p) {
-    list l = seznam;
-    while(l->next != NULL) {
-        
-        if(l->next->predmet.cena == p.cena && l->next->predmet.prostornina == p.prostornina) {
-            l->next->next = l->next->next->next;
-            l->next->predmet = l->next->next->predmet;
-            break;
-        }
+    for (int i = 0; i < numOfObjects; i++)
+    {
+        scanf("%d", &prices[i]);
     }
-    return l;
-}
-
-node *optimizirajSeznam(node *seznam1, node *seznam2) {
-    list l1 = seznam1;
-    while(l1->next != NULL) {
-        l1 = l1->next;
-        list l2 = seznam2;
-        while( l2->next != NULL) {
-            if(l2->predmet.prostornina == l1->predmet.prostornina) {
-                deleteElement(l2, l2->predmet);
-            }
-        }
-    }
-}
-
-node *mergeLists(node *l1, node *l2) {
-    list l = l1;
-    list s = l2;
-    while(s != NULL) {
-        l = insertElement(s->predmet, l);
-        s = s->next;
-    }
-    return l;
-}
- //uredi nekak 
-node *uredi(node *l1, node *l2, int V) {
-    list a = l1;
-    while(a != NULL) {
-        list b = l2;
-        int cena1 = a->predmet.cena;
-        int prostornina1 = a->predmet.prostornina;
-        while(b != NULL) {
-            int cena2 = b->predmet.cena;
-            int prostornina2 = b->predmet.prostornina;
-            if(cena1 == cena2 && prostornina1 == prostornina2 || prostornina1 > prostornina2 && cena1 == cena2 || prostornina2 > V || cena1 > cena2 && prostornina1 == prostornina2) {
-                l2 = deleteElement(l2, b->predmet);
-            } else if(prostornina2 > prostornina1 && cena1 == cena2 || cena2 > cena1 && prostornina1 == prostornina2)  {
-                l1 = deleteElement(l1, a->predmet);
-            }
-            b = b->next;
-        }
-        a = a->next;
-    }
-    return mergeLists(l1, l2);
-}
-
-void iteracije(struct predmet nahrbtnik[], int n, int V) {
-    list l1 = NULL;
-    
-    struct predmet novPredmet;
-    novPredmet.prostornina = 0;
-    novPredmet.cena = 0;
-    l1 = insertElement(novPredmet, l1);
-    for(int i = 0; i <= 1; i++) {
-        node *lIter = l1;
-        list l2 = NULL;
-        do {
-            struct predmet novPredmet;
-            novPredmet.prostornina = lIter->predmet.prostornina + nahrbtnik[i].prostornina;
-            novPredmet.cena = lIter->predmet.cena + nahrbtnik[i].cena;
-            l2 = insertElement(novPredmet, l2);
-            lIter = lIter->next;
-        }
-        while(lIter != NULL);
-        uredi(l1, l2, V);
-        l1 = mergeLists(l1, l2);
-        printRek(l1);
-        
-    }
-
-}
-
-int main() {
-    int V, n;
-    scanf("%d %d", &V, &n);
-    struct predmet nahrbtnik[n];
-    for(int i = 0; i < n; i++) {
-        struct predmet nov;
-        scanf("%d", &nov.prostornina);
-        nahrbtnik[i] = nov;
-    }
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &(nahrbtnik[i].cena));
-    }
-    for(int i = 0; i < n; i++) {
-        printf("%d %d\n", nahrbtnik[i].prostornina, nahrbtnik[i].cena);
-    }
-    list l = NULL;
-    l = insertElement(nahrbtnik[1], l);
-    printRek(l);
-    iteracije(nahrbtnik, n, V);
-
+    int result = backpack(numOfObjects, volumes, prices, 0, volume);
+    printf("%d", result);
     return 0;
 }
---
+++ VAJE/VAJE_08/nahrbtnik2/nahrbtnik2.c
@@ -0,0 +1,100 @@
+
+/*
+ * Re"sitev naloge Problem nahrbtnika 2
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*
+ * MEMO[ix][zeDodaliLiho][vNah]: "ze izra"cunana vrednost funkcije
+ * poisci(..., ix, zeDodaliLiho, vNah) oziroma 0, "ce te vrednosti "se nismo
+ * izra"cunali
+ */
+int MEMO[1001][2][1001];
+
+/*
+ * Vrne ceno optimalnega nahrbtnika s (preostalo) prostornino vNah, pri "cemer
+ * lahko vanj dodajamo predmete z indeksi od vklju"cno ix naprej (predmete z
+ * ni"zjimi indeksi smo obravnavali na prej"snjih nivojih rekurzije), poleg
+ * tega pa moramo upo"stevati tudi parameter zeDodaliLiho.  "Ce ima ta
+ * parameter vrednost true, potem smo v nahrbtnik "ze dodali predmet z liho
+ * prostornino (in takega predmeta ne smemo ve"c dodati), sicer pa ga "se
+ * nismo (zato tak predmet "se lahko dodamo).
+ *
+ * stPredmetov: "stevilo vseh predmetov
+ * prostornine: prostornine vseh predmetov
+ * cene: cene vseh predmetov
+ * ix: indeks trenutno obravnavanega predmeta
+ * zeDodaliLiho: true natanko v primeru, "ce nahrbtnik "ze vsebuje predmet z
+ *               liho prostornino
+ * vNah: preostala prostornina v nahrbtniku
+ */
+
+int poisci(int stPredmetov, int* prostornine, int* cene, int ix, bool zeDodaliLiho, int vNah) {
+    // nimamo ve"c predmetov, ki bi jih "se lahko dodali v nahrbtnik, zato
+    // cena takega nahrbtnika (oz. mno"zice predmetov) zna"sa 0
+    if (ix == stPredmetov) {
+        return 0;
+    }
+
+    // "ce smo vrednost funkcije poisci(..., ix, zeDodaliLiho, vNah) "ze
+    // izra"cunali, jo kar vrnemo
+    if (MEMO[ix][zeDodaliLiho][vNah] > 0) {
+        return MEMO[ix][zeDodaliLiho][vNah];
+    }
+
+    // mo"znost 1: predmeta z indeksom ix ne dodamo v nahrbtnik
+    int najCena = poisci(stPredmetov, prostornine, cene, ix + 1, zeDodaliLiho, vNah);
+
+    bool lihaProstornina = (prostornine[ix] % 2 == 1);
+
+    // mo"znost 2: predmet z indeksom ix dodamo v nahrbtnik, "ce sta
+    // izpolnjena slede"ca pogoja:
+    // -- v nahrbtniku je dovolj prostora za ta predmet
+    // -- predmet ima sodo prostornino ali pa nahrbtnik "se ne vsebuje
+    //    predmeta z liho prostornino
+
+    if (prostornine[ix] <= vNah && (!lihaProstornina || !zeDodaliLiho)) {
+        // mo"znost 2: predmet z indeksom ix dodamo v nahrbtnik
+        int c = cene[ix] + poisci(stPredmetov, prostornine, cene, ix + 1,
+                                  zeDodaliLiho || lihaProstornina, vNah - prostornine[ix]);
+
+        // izberemo bolj"so izmed obeh mo"znosti
+        if (c > najCena) {
+            najCena = c;
+        }
+    }
+
+    // memoiziramo
+    MEMO[ix][zeDodaliLiho][vNah] = najCena;
+
+    return najCena;
+}
+
+int main() {
+    int vNah;
+    scanf("%d", &vNah);
+
+    int stPredmetov;
+    scanf("%d", &stPredmetov);
+
+    int* prostornine = malloc(stPredmetov * sizeof(int));
+    for (int i = 0;  i < stPredmetov;  i++) {
+        scanf("%d", &prostornine[i]);
+    }
+
+    int* cene = malloc(stPredmetov * sizeof(int));
+    for (int i = 0;  i < stPredmetov;  i++) {
+        scanf("%d", &cene[i]);
+    }
+
+    int najCena = poisci(stPredmetov, prostornine, cene, 0, false, vNah);
+    printf("%d\n", najCena);
+
+    free(prostornine);
+    free(cene);
+
+    return 0;
+}
